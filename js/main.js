611696d0ad94d1750bff8e05447e455365d6d401
alejandrogoscu <alejandrogoscu@gmail.com>
2025-04-24 13:04:25 +0200

    js Version 0.1

--- js/main.js
+++ js/main.js
@@ -8,7 +8,22 @@ burger.addEventListener("click", () => {
   navMenu.classList.toggle("nav__menu--active");
 })
 
+// Cerrar el menu al hacer click en los enlaces.
 document.querySelectorAll(".menu__link").forEach(n => n.addEventListener("click", () => {
   burger.classList.remove("burger--active");
   navMenu.classList.remove("nav__menu--active");
-}))
+})) 
+
+// Cerrar el menu al hacer click fuera. 
+  document.addEventListener("click", e => {
+    if(!navMenu.contains(e.target) && e.target !== burger) {
+      burger.classList.remove("burger--active")
+      navMenu.classList.remove("nav__menu--active")
+    }
+  })
+
+// AÑADIR BORDE ROJO FORM INPUT INVALID
+document.getElementById("form__button").addEventListener("click", () => {
+  document.getElementById("contacto__form").className="form__submitted"
+})
+
